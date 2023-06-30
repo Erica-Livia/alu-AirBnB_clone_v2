f2f422406d1904192324589f68bbad9d049a65a4
BevilMulor <109423584+BevilMulor@users.noreply.github.com>
2023-06-30 22:35:51 +0200

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,47 +1,49 @@
 #!/usr/bin/python3
-"""simple flask routes"""
-from flask import Flask, abort
+""" Web application listening on 0.0.0.0, port 5000 """
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """return message"""
+@app.route("/")
+def hello_hbnb():
+    """ Display Hello HBNB! """
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb")
 def hbnb():
-    """return message HBNB"""
+    """ Display HBNB! """
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_fun(text):
-    """return message with paramns"""
-    new_text = text.replace("_", " ")
-    return "C {}".format(new_text)
+@app.route("/c/<text>")
+def c_is_fun(text):
+    """
+    Display 'C' followed by the value of text
+    Replace underscore _ symbols with a space
+    """
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/', defaults={'text': 'is_cool'})
-@app.route('/python', defaults={'text': 'is_cool'})
-@app.route('/python/<text>', strict_slashes=False)
-def python_fun(text):
-    """return message with paramns"""
-    new_text = text.replace("_", " ")
-    return "C {}".format(new_text)
+@app.route("/python/")
+@app.route("/python/<text>")
+def python_is_cool(text="is cool"):
+    """
+    Display 'Python' followed by the value of text
+    Replace underscore _ symbols with a space
+    """
+    return "Python {}".format(text.replace("_", " "))
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route("/number/<int:n>")
 def number(n):
-    """return message with paramns"""
-
-    if n.isdigit():
-        return "{} is a number".format(n)
-    else:
-        abort(404)
+    """
+    Display 'n is a number' only if n is an integer
+    """
+    return "{} is a number".format(n)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
+    app.url_map.strict_slashes = False
