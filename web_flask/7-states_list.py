6b7a85eb8c614714494690644ebfe062e5339c52
ericaing <109512001+Erica-Livia@users.noreply.github.com>
2023-06-30 23:56:02 +0200

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-
-"""Starts a Flask web application"""
-
+"""simple flask routes"""
+from flask import Flask, abort, render_template
 from models import storage
 from models.state import State
-from flask import Flask
-from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def states():
-    """Comment"""
-    states = storage.all('State').values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+def number_odd_or_even():
+    """return message with paramns"""
+    states = storage.all(State)
+    states_list = []
+    for i in states.keys():
+        states_list.append(states[i])
+    sorted_list = sorted(states_list, key=lambda x: x['name'])
+    return render_template('7-states_list.html',
+                           states=sorted_list)
 
 
 @app.teardown_appcontext
 def teardown(self):
-    """Remove the current SQLAlchemy Session"""
+    """Removes the current SQLAlchemy Session"""
     storage.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
